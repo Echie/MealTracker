6feff45ba34be11c8cfb8752d31970b7ef758a42
Teemu Huovinen <teemu@swappie.com>
2020-05-18 08:52:12 +0300

    Update urls, put meals at index

--- config/urls.py
+++ config/urls.py
@@ -3,15 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
+from meals.views import index
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path("meals/", include("mealtracker.meals.urls"), name="meals"),
-    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
+    path("", index, name="home"),
+    # path("", include("mealtracker.meals.urls"), name="meals"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
--- mealtracker/meals/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from meals.views import index
-
-urlpatterns = [
-    path("", index, name="index"),
-]
