117fcfdfeee76af8829aca558a08675b22097c96
Teemu Huovinen <teemu@swappie.com>
2021-04-11 14:11:44 +0300

    Add button for copying ingredients to clipboard

--- mealtracker/static/js/project.js
+++ mealtracker/static/js/project.js
@@ -1,1 +1,18 @@
 /* Project specific Javascript goes here. */
+
+$("button.copy-to-clipboard").click((e) => {
+  const ingredientElements = $(e.target).parent().children("li");
+  const ingredientStr = ingredientElements
+    .map((_, el) => el.textContent)
+    .toArray()
+    .join("\n");
+  copyStringToClipboard(ingredientStr);
+});
+
+function copyStringToClipboard(text) {
+  var input = $("<textarea>");
+  $("body").append(input);
+  input.val(text).select();
+  document.execCommand("copy");
+  input.remove();
+}
